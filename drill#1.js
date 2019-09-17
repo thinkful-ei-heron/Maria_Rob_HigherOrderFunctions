337ab044a7b788e1a2ef73ca4fc19a96c6893099
robarcand <robbyarcand@aol.com>
2019-09-17 15:17:57 -0400

    finished drill#1.js

--- drill#1.js
+++ drill#1.js
@@ -1,16 +1,34 @@
-'strict mode'
+'use strict';
 
-function repeat (fn, n){
-    for (i=0; i< n, i++){
-        fn ()
-    }
-    const hello = function (){
-        console.log('Hello world ')
+function repeat(fn, n) {
+    for(let i = 0; i < n; i++) {
+        fn();
     }
 
-    const goodbye = function () {
-        console.log('Goodbye world')
-
-    }
 }
-console.log(repeat (hello, 5));   
+
+let hello = function() {
+    console.log('Hello world');
+};
+
+let goodbye = function() {
+    console.log('Goodbye world');
+};
+
+repeat(hello, 5);
+
+/* function repeat(fn, n) {
+
+  for (let i = 0; i < n; i++) {
+    fn();
+  }
+  const hello = function() {
+    console.log('Hello world');
+  };
+
+  const goodbye = function () {
+    console.log('Goodbye world');
+
+  };
+}
+console.log(repeat (hello(), 5));   */
